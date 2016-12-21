8e7e7982590c47253a5623dc966a45754fd1d499
huangzhike <heavenlystate@163.com>
2016-12-21 15:41:29 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
+
 # offCanvas
-a OffCanvas component
+
+* 类似原生应用的汉堡菜单，点击开关侧边栏菜单的组件，目前实现效果：
+	* push：点击按钮，侧边栏panel与主界面同时translate；
+		* 原理：侧边栏与主界面在同一容器中，可以是body或别的，侧边栏在视野外，激活时容器整体translate，露出侧边栏，同时`overflow-x: hidden`
+	* overlay：点击呼出侧边栏panel，主界面不动；
+		* 原理：同上，只不过主界面不动，侧边栏translate
+	* reveal：点击使主界面移动，reveal出下面的侧边栏panel；
+		* 原理：侧边栏在主界面下（z-index小），激活时主界面translate，露出侧边栏
+
+* 使用方法：
+	* 引入offcanvas.css
+	* 引入OffCanvas.js
+	* `new OffCanvas`调用：
+	```
+var yourName = new OffCanvas({
+		// 侧边栏panel的html，自定义
+		html: "",
+		// 侧边栏类型，字符串
+		type: "offcanvas-push",
+		// DOM容器
+		wraper: wraper
+	});
+
+// 给DOMElement添加事件处理
+DOMElement.addEventListener("click", yourName.handleEvent.bind(yourName));
+```
+* 请使用高级浏览器；
+* 别看js50行不到，踩了好多坑。。。
---
+++ js/OffCanvas.js
@@ -0,0 +1,52 @@
+(function() {
+
+	var OffCanvas = function(props) {
+			// 侧边栏内容
+			this.html = props.html || "";
+			// 类型
+			this.type = props.type;
+			// 容器
+			this.wraper = props.wraper;
+			this.open = false;
+			// 不同类型OffCanvas，插入的父节点
+			this.par = (this.type == "offcanvas-overlay" || this.type == "offcanvas-push") ? this.wraper : document.body;
+		};
+
+	OffCanvas.prototype.create = function() {
+		if (this.open) return;
+		var OffC = document.createElement('div');
+		OffC.innerHTML = this.html;
+		OffC.className = "offcanvas " + this.type;
+		// remove时用
+		this.OffC = OffC;
+		// 添加或移除类的容器
+		this.act = this.type == "offcanvas-overlay" ? this.OffC : this.wraper;
+		// 插入DOM
+		this.par.appendChild(OffC);
+		console.log("add");
+		// reflow触发动画
+		OffC.offsetWidth = OffC.offsetWidth;
+		this.act.classList.add('OffC-active');
+		this.open = true;
+	};
+
+	OffCanvas.prototype.close = function() {
+		if (!this.open) return;
+		this.act.classList.remove('OffC-active');
+		var that = this;
+		// 移除
+		setTimeout(function() {
+			that.par.removeChild(that.OffC);
+			console.log("remove");
+		}, 200);
+		this.open = false;
+	};
+
+	OffCanvas.prototype.handleEvent = function() {
+		// 事件处理
+		this.open ? this.close() : this.create();
+	};
+
+	return window.OffCanvas = OffCanvas;
+
+})();
