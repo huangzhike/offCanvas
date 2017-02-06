89ab781cd0385f4fdcc3a0d8e7f3884320492429
huangzhike <heavenlystate@163.com>
2017-02-06 18:57:19 +0800

    一些更改

--- js/OffCanvas.js
+++ js/OffCanvas.js
@@ -1,16 +1,16 @@
 (function() {
 
 	var OffCanvas = function(props) {
-			// 侧边栏内容
-			this.html = props.html || "";
-			// 类型
-			this.type = props.type;
-			// 容器
-			this.wraper = props.wraper;
-			this.open = false;
-			// 不同类型OffCanvas，插入的父节点
-			this.appendedParent = (this.type == "offcanvas-overlay" || this.type == "offcanvas-push") ? this.wraper : document.body;
-		};
+		// 侧边栏内容
+		this.html = props.html || "";
+		// 类型
+		this.type = props.type;
+		// 容器
+		this.wraper = props.wraper;
+		this.open = false;
+		this.create();
+
+	};
 
 	OffCanvas.prototype.create = function() {
 		if (this.open) return;
@@ -21,6 +21,9 @@
 		this.panel = panel;
 		// 添加或移除类的容器
 		this.act = this.type == "offcanvas-overlay" ? this.panel : this.wraper;
+
+		// 不同类型OffCanvas，插入的父节点
+		this.appendedParent = (this.type == "offcanvas-overlay" || this.type == "offcanvas-push") ? this.wraper : document.body;
 		// 插入DOM
 		this.appendedParent.appendChild(panel);
 		console.log("add");
@@ -28,6 +31,8 @@
 		panel.offsetWidth = panel.offsetWidth;
 		this.act.classList.add("active");
 		this.open = true;
+		document.addEventListener("click", this, false);
+
 	};
 
 	OffCanvas.prototype.close = function() {
@@ -40,13 +45,19 @@
 			console.log("remove");
 		}, 200);
 		this.open = false;
+		document.removeEventListener("click", this, false);
 	};
 
-	OffCanvas.prototype.handleEvent = function() {
+	OffCanvas.prototype.handleEvent = function(e) {
+
 		// 事件处理
-		this.open ? this.close() : this.create();
+		var ele = e.target;
+		if (this.open && ele !== this.panel && e.target.className !== "btn") {
+			this.close();
+		}
+
 	};
 
-	return window.OffCanvas = OffCanvas;
+	window.OffCanvas = OffCanvas;
 
 })();
