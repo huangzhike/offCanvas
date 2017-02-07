ad0dc8318c7d139c7482bc9351252dfb82045a22
huangzhike <heavenlystate@163.com>
2017-02-07 16:04:28 +0800

    一些美化

--- js/OffCanvas.js
+++ js/OffCanvas.js
@@ -5,54 +5,57 @@
 		this.html = props.html || "";
 		// 类型
 		this.type = props.type;
-		// 容器
+		// 父容器
 		this.wraper = props.wraper;
-		this.open = false;
-		this.create();
 
 	};
 
 	OffCanvas.prototype.create = function() {
-		if (this.open) return;
+		if (this.created == true) return;
+
+		console.log("created");
+
 		var panel = document.createElement("div");
 		panel.innerHTML = this.html;
 		panel.className = "offcanvas " + this.type;
 		// remove时用
 		this.panel = panel;
-		// 添加或移除类的容器
-		this.act = this.type == "offcanvas-overlay" ? this.panel : this.wraper;
 
 		// 不同类型OffCanvas，插入的父节点
-		this.appendedParent = (this.type == "offcanvas-overlay" || this.type == "offcanvas-push") ? this.wraper : document.body;
+		this.appendedParent = this.type == "offcanvas-reveal" ? document.body : this.wraper;
 		// 插入DOM
 		this.appendedParent.appendChild(panel);
-		console.log("add");
-		// reflow触发动画
-		panel.offsetWidth = panel.offsetWidth;
-		this.act.classList.add("active");
-		this.open = true;
+		
+		// 添加或移除类的容器
+		this.active = this.type == "offcanvas-overlay" ? this.panel : this.wraper;
+		// overlay reflow
+		this.active.offsetWidth = this.active.offsetWidth;
+		this.active.classList.add("active");
+
 		document.addEventListener("click", this, false);
+		this.created = true;
 
 	};
 
 	OffCanvas.prototype.close = function() {
-		if (!this.open) return;
-		this.act.classList.remove("active");
+
+		this.active.classList.remove("active");
 		var that = this;
 		// 移除
 		setTimeout(function() {
 			that.appendedParent.removeChild(that.panel);
-			console.log("remove");
+			console.log("removed");
 		}, 200);
-		this.open = false;
+
 		document.removeEventListener("click", this, false);
+		this.created = false;
 	};
 
 	OffCanvas.prototype.handleEvent = function(e) {
 
 		// 事件处理
 		var ele = e.target;
-		if (this.open && ele !== this.panel && e.target.className !== "btn") {
+		if (ele !== this.panel && e.target.className !== "btn") {
 			this.close();
 		}
 
